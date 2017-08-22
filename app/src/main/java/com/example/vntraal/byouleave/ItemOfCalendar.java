153a9caaf50c27fecaca09123b32566589a9370e
João Victor Ignacio <vntjoig@venturus.local>
2017-08-22 11:35:17 -0300

    Started to work in repairs of the RecyclerView. Not Finished Yet

---
+++ app/src/main/java/com/example/vntraal/byouleave/ItemOfCalendar.java
@@ -0,0 +1,13 @@
+package com.example.vntraal.byouleave;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemOfCalendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_of_calendar);
+    }
+}
