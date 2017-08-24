19dcda49f1c2810055fc33746c68e703cdb20b14
João Victor Ignacio <vntjoig@venturus.local>
2017-08-24 07:51:47 -0300

    RecyclerView Working

--- app/src/main/java/com/example/vntraal/byouleave/EventAdapter.java
+++ app/src/main/java/com/example/vntraal/byouleave/EventAdapter.java
@@ -1,8 +1,58 @@
 package com.example.vntraal.byouleave;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by vntjoig on 23/08/2017.
  */
 
-public class EventAdapter {
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder> {
+
+    private ArrayList<String> mData = new ArrayList<String>();
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView mTextView;
+
+        public ViewHolder(TextView v){
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    public void setmData(ArrayList<String> received){
+        mData = received;
+        notifyDataSetChanged();
+        for (String event : mData){
+            Log.e("Member name: ", event);
+        }
+
+    }
+
+    @Override
+    public EventAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        TextView v = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.event_list_item, parent, false);
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(EventAdapter.ViewHolder holder, int position) {
+        holder.mTextView.setText(mData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
 }
