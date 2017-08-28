484837691ff60c229aade9ffea2e1d953d8c7220
Jonaphael <jonaphael.aristil@venturus.org.br>
2017-08-28 08:03:47 -0300

    Wifi Configuration on android and ESP8266

--- app/src/main/java/com/example/vntraal/byouleave/WifiConnection.java
+++ app/src/main/java/com/example/vntraal/byouleave/WifiConnection.java
@@ -2,6 +2,8 @@ package com.example.vntraal.byouleave;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
@@ -10,15 +12,22 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
-import java.lang.reflect.Method;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
@@ -33,11 +42,17 @@ public class WifiConnection extends AppCompatActivity {
     Context myContext;
     WifiConfiguration myWifiConfiguration;
     public static String netWorkSSID = "Jonaphael ESP";
-    public static  String  netWorkPass="esp82660";
-    byte[] buf = new byte[1024]; // to send Informations
+    public static  String  netWorkPass="esp8266esp";
+    byte[] buf = new byte[50]; // to recieve Informations
+
+    Socket socket;
+    String ip = "192.168.137.56";
+    int port = 12345;
+    BufferedReader bufferedReader;
 
     //Components Layout
     Button myTurnButton;
+    TextView textESP;
 
 
     @Override
@@ -54,78 +69,58 @@ public class WifiConnection extends AppCompatActivity {
         StrictMode.ThreadPolicy myThreadPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(myThreadPolicy);
 
+        //Textfrom esp
+        textESP = (TextView)findViewById(R.id.txtESP);
+
         // TURN BUTTON
         myTurnButton =  (Button) findViewById(R.id.turn);
         myTurnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-               // if the wifi is on turn it off
-                if(myWifiManager.isWifiEnabled()){
-                    myWifiManager.setWifiEnabled(false);
-                }
-                // setting the permissions to WRITE SSETTINGS
-                settingPermission();
-                setConfigurations();
-
-                try{
-                    activate();
-                }
-                catch (Exception e){
+                try {
+                    readMessages();
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
+                // wifiConnect();
             }
         });
     }
 
-    public void setConfigurations(){
+    public void readMessages() throws Exception {
+        try {
 
-        myWifiConfiguration = new WifiConfiguration();
+            socket = new Socket(ip,port);
+            Log.e("e","Conectado");
 
-        if(netWorkPass==""){
-            myWifiConfiguration.SSID = netWorkSSID;
-            myWifiConfiguration.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
-            myWifiConfiguration.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
-            myWifiConfiguration.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
-            myWifiConfiguration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+            if(socket.isConnected())
+                Log.e("h","Socket Conectado");
+
+           // bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DataInputStream dataInputStream;
+
+            //este meetodo fica em loop eternamente - tranformar esta l[ogica em um serviço
+            while(true) {
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                int result = dataInputStream.read(buf);
+                String ret2 = new String(buf);
+                Log.e("e","out1S "+ret2);
+                Log.e("e","outINT "+result);
+                textESP = (TextView)findViewById(R.id.txtESP);
+                textESP.setText(ret2);
+            }
+
         }
-        else {
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            Toast.makeText(getApplicationContext(),"Ending Connection", Toast.LENGTH_SHORT).show();
+            socket.close();
 
-            myWifiConfiguration.SSID = netWorkSSID;
-            myWifiConfiguration.preSharedKey = netWorkPass;
-            myWifiConfiguration.status = WifiConfiguration.Status.ENABLED;
-            myWifiConfiguration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
-            myWifiConfiguration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
-            myWifiConfiguration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
-            myWifiConfiguration.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
-            myWifiConfiguration.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
-            myWifiConfiguration.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
-            myWifiConfiguration.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
-         //   myWifiConfiguration.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
-          //  myWifiConfiguration.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
-           // myWifiConfiguration.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
-            //myWifiConfiguration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
-            //myWifiConfiguration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
-           // myWifiConfiguration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
-           // myWifiConfiguration.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
-            //myWifiConfiguration.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
         }
     }
 
-    public void activate() throws Exception{
-        Method setWifiApMethod = myWifiManager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
-      /*  boolean apstatus=(Boolean) setWifiApMethod.invoke(myWifiManager, myWifiConfiguration,true);
-
-        Method isWifiApEnabledmethod = myWifiManager.getClass().getMethod("isWifiApEnabled");
-        while(!(Boolean)isWifiApEnabledmethod.invoke(myWifiManager)){};
-        Method getWifiApStateMethod = myWifiManager.getClass().getMethod("getWifiApState");
-        int apstate=(Integer)getWifiApStateMethod.invoke(myWifiManager);
-        Method getWifiApConfigurationMethod = myWifiManager.getClass().getMethod("getWifiApConfiguration");
-        myWifiConfiguration=(WifiConfiguration)getWifiApConfigurationMethod.invoke(myWifiManager);*/
-        setWifiApMethod.invoke(myWifiManager, myWifiConfiguration,false);
-        myWifiManager.saveConfiguration();
-        Toast.makeText(getApplicationContext(),"SSID:"+myWifiConfiguration.SSID+"Password:"+myWifiConfiguration.preSharedKey+"", Toast.LENGTH_SHORT).show();
-    }
 
     // TO TURN THE WIFI ON OR OFF
     public static void turnONOFFWifi(Context context, boolean value){
@@ -150,7 +145,7 @@ public class WifiConnection extends AppCompatActivity {
         if (turn){
             turnONOFFWifi(myContext, turn);
             turn = false;
-            Toast.makeText(getApplicationContext(),"Turning on wifi ...", Toast.LENGTH_SHORT).show();
+            Toast.makeText(myContext,"Turning on wifi ...", Toast.LENGTH_SHORT).show();
 
             // wifi configuration
             myWifiConfiguration = new WifiConfiguration();
@@ -172,49 +167,27 @@ public class WifiConnection extends AppCompatActivity {
         else{
             turnONOFFWifi(myContext,turn);
             turn = true;
-            Toast.makeText(getApplicationContext(),"Turning off wifi ...", Toast.LENGTH_SHORT).show();
+            Toast.makeText(myContext,"Turning off wifi ...", Toast.LENGTH_SHORT).show();
         }
-    }
 
-    //to Turn the wifi on or Off
-  /*  public static void turnONOFFWifi(Context context, boolean value){
-        myWifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-        myWifiManager.setWifiEnabled(value);
-    }
-    */
 
-    //to send datato esp
-    public class Client implements Runnable{
-        private final static String SERVER_ADRESS = "";
-        private final static int    SERVER_PORT = 8888;
+        try {
+            Thread.sleep(20000);
+            ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 
-        @Override
-        public void run() {
-            InetAddress serverAdress;
-            DatagramPacket dtPacket;
-            DatagramSocket dtSocket = null;
+            if (mWifi.isConnected()) {
+                readMessages();
+            }
+            else
+            {
+               Log.e("erro", "Nao se Conectou na rede ainda");
+            }
 
-            try{
-                serverAdress = InetAddress.getByName(SERVER_ADRESS);
-                dtSocket = new DatagramSocket();
-                dtPacket = new DatagramPacket(buf, buf.length, serverAdress, SERVER_PORT);
-                dtSocket.send(dtPacket);
-            }
-            catch (UnknownHostException err){
-                err.printStackTrace();
-            }
-            catch (SocketException err){
-                err.printStackTrace();
-            }
-            catch (IOException err){
-                err.printStackTrace();
-            }
-            finally {
-                if(!dtSocket.isClosed()){
-                    dtSocket.close();
-                }
-            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 
 }
